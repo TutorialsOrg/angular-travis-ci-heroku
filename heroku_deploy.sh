dd3dce1006c187fbc8e84473ee17c10389e02748
preetham <preetham.salehundam@gmail.com>
2017-08-17 17:52:03 +0530

    heroku commit

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
-cd $1;
 git init;
 git config user.name "preetham" && git config user.email "preetham.salehundam@gmail.com";
-if [ -z "$2" ]
+if [ -z "$1" ]
 then
     heroku create
 else
-    heroku create $2
+    heroku create $1
 fi
 git add .;
 git commit -m "heroku commit";

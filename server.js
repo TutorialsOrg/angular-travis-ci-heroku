c95fbd74c1e40101ebe3fd320d416b86a186d82a
Preetham <preetham_2971993@yahoo.co.in>
2017-08-17 17:29:07 +0530

    initial heroku deploy

--- server.js
+++ server.js
@@ -16,4 +16,6 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
   
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,function(){
+  console.log("server started successfully!")
+});

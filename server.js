569d13f7e612f1c5c6cf3d28f8408329b1f2875c
Preetham <preetham_2971993@yahoo.co.in>
2017-08-17 17:42:24 +0530

    changed port config

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
   
-app.listen(process.env.PORT || 9000,function(){
-  console.log("server started successfully!")
+
+
+var server_port = process.env.PORT || 8080;
+var server_host = '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });

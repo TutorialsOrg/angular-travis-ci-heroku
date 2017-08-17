04e5fc7df98af0a6ff1c760fa187f5b75afb62d8
preetham <preetham.salehundam@gmail.com>
2017-08-17 23:15:52 +0530

    removed heroku script added native heroku provider for travis

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
-git init;
-git config user.name "preetham" && git config user.email "preetham.salehundam@gmail.com";
-if [ -z "$1" ]
-then
-    heroku create
-else
-    heroku create $1
-fi
-git add .;
-git commit -m "heroku commit";
-git push heroku master
+# git init;
+# git config user.name "preetham" && git config user.email "preetham.salehundam@gmail.com";
+# if [ -z "$1" ]
+# then
+#     heroku create
+# else
+#     heroku create $1
+# fi
+# git add .;
+# git commit -m "heroku commit";
+# git push heroku master
 
